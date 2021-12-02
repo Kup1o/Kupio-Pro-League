be02c6e51b8bc672b3e5887941b2b9341096b45d
Kup1o <nikitaack@yandex.ru>
2021-12-03 00:04:51 +0900

    Add Linux Support

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-srcds.exe -game csgo -console -usercon -maxplayers_override 12 -tickrate 128 -port 27016 +game_type 0 +game_mode 1 -insecure +sv_lan 0 -nobots
+srcds.exe -game csgo -console -usercon -maxplayers_override 12 -tickrate 128 -port 27015 +game_type 0 +game_mode 1 -insecure +sv_lan 0 -nobots
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./srcds_run -game csgo -console -usercon -maxplayers_override 12 -tickrate 128 -port 27015 +game_type 0 +game_mode 1 -insecure +sv_lan 0 -nobots
